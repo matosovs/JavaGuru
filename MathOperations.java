f8be94a47db0e2376782be67857712d4506a7f72
matosovs <44256622+matosovs@users.noreply.github.com>
2018-10-18 11:09:04 +0300

    Add files via upload

---
+++ MathOperations.java
@@ -0,0 +1,16 @@
+package lv.javaguru.demo;
+
+public class MathOperations {
+    public static void main(String[] args){
+        int firstNumber = 25;
+        int secondNumber = 5;
+        int sumResult = firstNumber + secondNumber;
+        int subtractResult = firstNumber - secondNumber;
+        int divideResult = firstNumber / secondNumber;
+        int multiplyResult = firstNumber * secondNumber;
+        System.out.println("Sum result: "+ sumResult);
+        System.out.println("Subtract result: "+ subtractResult);
+        System.out.println("Divide result: "+ divideResult);
+        System.out.println("Multiply result "+ multiplyResult);
+    }
+}
